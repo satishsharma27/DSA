39877cb2fec806acd3b856f6e9ffee8c3b48899a
satishsharma27 <satish.sharma2709@gmail.com>
2026-02-02 23:32:32 +0530

    done

--- DSA.py
+++ DSA.py
@@ -1,3 +1,4 @@
 print("Hello, World!")
 print("This is a sample DSA module.")
 print("DSA stands for Data Structures and Algorithms.")
+print("Let's implement some basic data structures and algorithms here.")
